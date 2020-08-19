98f9b14dda7bcc495b86844af38a828e6ce9bae2
Liquid Solid <liquid.solid@protonmail.com>
2020-08-19 20:39:52 +0300

    Tools tested

--- src/__tests__/tools.spec.ts
+++ src/__tests__/tools.spec.ts
@@ -1,33 +1,33 @@
 import flushPromises from 'flush-promises';
 import {
-  computeRippleRadius, nextFrame
+  computeRippleRadius, nextFrame, centerCoords, relativeMouseCoords, consoleWarn, applyStyles
 } from '../tools';
 
 describe('Tools', () => {
   describe('computeRippleRadius()', () => {
-    /**
-     * Формула из теоремы пифагора
-     */
-    function pythagoras(a: number, b: number): number {
-      return (a ** 2 + b ** 2) ** 0.5;
-    }
+    // /**
+    //  * Формула из теоремы пифагора
+    //  */
+    // function pythagoras(a: number, b: number): number {
+    //   return (a ** 2 + b ** 2) ** 0.5;
+    // }
 
     /**
-     * description | width, height | x, y | expected x, y
+     * description | width, height | x, y
      */
-    type Case = [string, number, number, number, number, number, number];
+    type Case = [string, number, number, number, number];
 
     const cases: Case[] = [
-      ['Center', 500, 500, 250, 250, 250, 250],
-      ['Sector I', 100, 100, 60, 20, 60, 80],
-      ['Sector II', 100, 100, 30, 20, 70, 80],
-      ['Sector III', 100, 100, 10, 80, 90, 80],
-      ['Sector IV', 100, 100, 90, 60, 90, 60]
+      ['Center', 500, 500, 250, 250],
+      ['Sector I', 100, 100, 60, 20],
+      ['Sector II', 100, 100, 30, 20],
+      ['Sector III', 100, 100, 10, 80],
+      ['Sector IV', 100, 100, 90, 60]
     ];
 
-    cases.forEach(([description, w, h, x, y, a, b]) => {
+    cases.forEach(([description, w, h, x, y]) => {
       it(description, () => {
-        expect(computeRippleRadius(w, h, x, y)).toEqual(pythagoras(a, b));
+        expect(computeRippleRadius(w, h, x, y)).toMatchSnapshot();
       });
     });
   });
@@ -52,8 +52,9 @@ describe('Tools', () => {
        * Эта функция имитирует запуск всех колбеков
        */
       const next = () => {
-        queue.forEach((x) => x());
+        const current = [...queue];
         queue = [];
+        current.forEach((x) => x());
       };
 
       return {
@@ -130,6 +131,82 @@ describe('Tools', () => {
       await flushPromises();
 
       expect(isPromiseDone).toBe(true);
+    });
+  });
+
+  describe('centerCoords()', () => {
+    it('Get center of element bounding rect', () => {
+      const el = document.createElement('div');
+      jest.spyOn(el, 'getBoundingClientRect').mockImplementation(
+        () => ({
+          x: 900,
+          y: 0,
+          width: 400,
+          height: 20
+        } as DOMRect)
+      );
+
+      const [cx, cy] = centerCoords(el);
+
+      expect(cx).toEqual(200);
+      expect(cy).toEqual(10);
+    });
+  });
+
+  describe('relativeMouseCoords()', () => {
+    it('Correctly compute mouse coords in element rect', () => {
+      const el = document.createElement('div');
+      const rect = {
+        left: 192,
+        top: 27,
+        x: 192,
+        y: 27,
+        width: 400,
+        height: 20
+      } as DOMRect;
+      jest.spyOn(el, 'getBoundingClientRect').mockImplementation(() => rect);
+      const mouse = {
+        clientX: 3,
+        clientY: 234
+      };
+
+      const [x, y] = relativeMouseCoords(el, mouse);
+
+      expect([x, y]).toMatchSnapshot();
+    });
+  });
+
+  describe('consoleWarn()', () => {
+    beforeEach(() => {
+      jest.spyOn(console, 'warn').mockImplementation(() => null);
+    });
+
+    afterEach(() => {
+      // eslint-disable-next-line no-console
+      (console.warn as jest.Mock).mockRestore();
+    });
+
+    it('Logging with [Ripple] prefix', () => {
+      const args = ['Hello', true, 'World!', 41234];
+      consoleWarn(...args);
+
+      // eslint-disable-next-line no-console
+      expect(console.warn).toHaveBeenCalledWith('[Ripple]', ...args);
+    });
+  });
+
+  describe('applyStyles()', () => {
+    it('Applying all provided styles', () => {
+      const el = document.createElement('div');
+      const styles: Partial<CSSStyleDeclaration> = {
+        height: '100px',
+        transition: 'all .41s ease',
+        color: 'red'
+      };
+
+      applyStyles(el, styles);
+
+      expect(getComputedStyle(el)).toMatchSnapshot();
     });
   });
 });
