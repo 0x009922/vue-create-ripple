6ca8f0d4e3aafddb91bae6f0bfc6650180e28fe8
0x009922 <a.marcius26@gmail.com>
2021-02-14 09:02:58 +0300

    build: it is building again

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,31 @@
-import ts from 'rollup-plugin-typescript2';
-import babel from '@rollup/plugin-babel';
+// import ts from 'rollup-plugin-typescript2';
+// import babel from '@rollup/plugin-babel';
 import dts from 'rollup-plugin-dts';
-import rimraf from 'rimraf';
+// import rimraf from 'rimraf';
 import pkg from './package.json';
 
-rimraf.sync('./dist');
+// rimraf.sync('./dist');
 
 export default [
-  {
-    input: 'src/index.ts',
-    output: {
-      file: pkg.main,
-      format: 'es'
-    },
-    plugins: [
-      ts({
-        tsconfigOverride: {
-          compilerOptions: {
-            removeComments: true
-          }
-        }
-      }),
-      babel({
-        extensions: ['.ts']
-      })
-    ]
-  },
+  // {
+  //   input: 'src/index.ts',
+  //   output: {
+  //     file: pkg.main,
+  //     format: 'es'
+  //   },
+  //   plugins: [
+  //     ts({
+  //       tsconfigOverride: {
+  //         compilerOptions: {
+  //           removeComments: true
+  //         }
+  //       }
+  //     }),
+  //     babel({
+  //       extensions: ['.ts']
+  //     })
+  //   ]
+  // },
   {
     input: 'src/index.ts',
     output: {
